38fbabcbba09f917023e1aa1183e79c6082f05ae
Spanchik <72700377+Spanchik@users.noreply.github.com>
2023-05-22 00:23:34 +0300

    Alarm.ino

--- Alarm.ino
+++ Alarm.ino
@@ -5,137 +5,149 @@ SoftwareSerial gsmModule(D7, D8);  // RX=D7, TX=D8 pins for GSM module
 const int motionPin = D3; // Motion sensor pin, connected to D3
 const int buzzerPin = D1; // Buzzer pin, connected to D1
 const int ledPin = D0; // LED pin, connected to D0
-const unsigned long BATTERY_CHECK_INTERVAL = 6 * 60 * 60 * 1000; // 6 hours in milliseconds
-unsigned long lastBatteryCheckTime = 0; // Variable to store the timestamp of the last battery check
-
+const unsigned long waitTime = 5000;  // Set the maximum waiting time for response
+const unsigned long checkInterval = 12 * 60 * 60 * 1000; // Battery level check interval of 12 hours
+unsigned long lastBatteryCheckTime = 0;
 
 bool isArmed = false; // Alarm status
-bool isMotionDetected = false; // Motion sensor status  
+bool isMotionDetected = false; // Motion sensor status
 
 String adminNumbers[] = {"+380681496848"};   // Record administrator phone numbers, you can add more than one
 
 
+// Function for sending SMS messages
 void sendSMS(String message) {
-  // Send an SMS to each admin number
+  // Iterate through each administrator number to send the SMS message to all administrators.
   for (int i = 0; i < sizeof(adminNumbers) / sizeof(adminNumbers[0]); i++) {
-    if (isAdminNumber(adminNumbers[i])) {
-      // Format the AT command to send an SMS
-      String command = "AT+CMGS=\"" + adminNumbers[i] + "\"\r\n";
-      // Send the command to the GSM module
-      gsmModule.print(command);
-      delay(1000);
-      // Send the SMS message
-      gsmModule.print(message);
-      delay(100);
-      // Send the Ctrl+Z character to indicate the end of the message
-      gsmModule.print((char)26);
-      delay(1000);
+    gsmModule.println("AT+CMGS=\"" + adminNumbers[i] + "\""); // Send command to set the SMS recipient
+    delay(1000); // Delay 1 second for the response
+    gsmModule.println(message); // Send the text of the SMS message
+    delay(1000);
+    gsmModule.println((char)26); // Send Ctrl+Z character to end the SMS message
+    delay(100);
+    while (gsmModule.available()) {
+      Serial.write(gsmModule.read()); // Output the response from the GSM module to Serial Monitor
     }
   }
 }
 
 
+//Initializes the system and sets up the necessary configurations.
 void setup() {
-  // Configure the serial port for communication with the GSM module
-  gsmModule.begin(9600);
+  Serial.begin(9600); // Initialize the Serial communication
+  delay(1000); // Delay 1 second for stability
+  
+  pinMode(motionPin, INPUT); // Set motionPin as input
+  pinMode(ledPin, OUTPUT); // Set ledPin as output
+  pinMode(buzzerPin, OUTPUT); // Set buzzerPin as output
+  
+  gsmModule.begin(9600); // Initialize the GSM module communication
   delay(1000);
-  // Send the command to set the SMS text mode
-  gsmModule.println("AT+CMGF=1\r\n"); // Set the text mode command
-  delay(500);
-  gsmModule.println("AT+CNMI=2,2,0,0,0\r\n");  // Set the notification for receiving a new SMS command
-  delay(500);
-  sendSMS("Alarm system initialized. Enter 'On' to arm the alarm and 'Off' to disarm.");
+  
+  gsmModule.println("AT"); // Send AT command to the GSM module
+  delay(1000); 
+  while (gsmModule.available()) {
+    Serial.write(gsmModule.read()); // Print the response from the GSM module to Serial Monitor
+  }
+  
+  gsmModule.println("AT+CMGF=1"); // Set SMS text mode
+  delay(1000); 
+  while (gsmModule.available()) {
+    Serial.write(gsmModule.read()); 
+  }
+  
+  gsmModule.println("AT+CNMI=1,2,0,0,0"); // Set up SMS notification mode
+  delay(1000); 
+  while (gsmModule.available()) {
+    Serial.write(gsmModule.read());
+  }
+
+   // Delete all SMS messages
+  gsmModule.println("AT+CMGD=1,4"); // Delete all messages in all storage locations
+  delay(1000);
+  while (gsmModule.available()) {
+    Serial.write(gsmModule.read());
+  }
+
+  sendSMS("Alarm system initialized. Enter 'On' to arm the alarm, 'Off' to disarm, and 'Battery' to check battery status.");
+  // Send an SMS to notify the administrators about the initialization and provide usage instructions.
 }
 
 
 void loop() {
+  
+  checkInput(); // check for user input and process commands
   checkMotionSensor(); // check motion sensor and send SMS if motion detected
-  checkSMS(); // check for user input and process commands
-
-  // Check battery level every 6 hours
+  // check battery level every 12 hours and send SMS if critically lowOff
   unsigned long currentTime = millis();
-  if (currentTime - lastBatteryCheckTime >= BATTERY_CHECK_INTERVAL) {
-    monitorBatteryLevel();
+  if (currentTime - lastBatteryCheckTime >= checkInterval) {
+    checkBattery();
     lastBatteryCheckTime = currentTime;
   }
 
 }
 
 
-bool isAdminNumber(String sender) {
-  // Check if the given sender is an admin number
-  for (int i = 0; i < sizeof(adminNumbers) / sizeof(adminNumbers[0]); i++) {
-    if (sender == adminNumbers[i]) {
-      return true;  // Sender is an admin number
-    }
-  }
-  return false;  // Sender is not an admin number
-}
+//Checks for incoming SMS messages and processes the commands.
+void checkInput() {
+  // Check if there is data available to read from the GSM module
+  if (gsmModule.available()) {
+  String receivedData = gsmModule.readStringUntil('\n'); // Read the data from the GSM module
 
+    if (receivedData.startsWith("+CMT:")) {
+      // Extract the sender number from the receivedData string
+      int firstIndex = receivedData.indexOf('"');
+      int secondIndex = receivedData.indexOf('"', firstIndex + 1);
+      String senderNumber = receivedData.substring(firstIndex + 1, secondIndex);
+      senderNumber.trim(); // Remove leading/trailing whitespaces
+      Serial.println("Admin number found: " + senderNumber);
 
-void checkSMS() {
-  // Check for incoming SMS messages
-  while (gsmModule.available()) {
-    String message = gsmModule.readStringUntil('\n');
-    message.trim();  // Remove leading and trailing whitespaces
-    if (message.length() > 0) {
-      // Check the sender of the message
-      if (isAdminNumber(message)) {
-        // Process the message from an admin
-        processAdminCommand(message);
+      // Read the SMS text
+      String sms = gsmModule.readStringUntil('\n');
+      Serial.println("SMS text: " + sms);
+      sms.trim();
+
+      // Check if the sender number is in the list of adminNumbers
+      bool isAdmin = false;
+      for (int i = 0; i < sizeof(adminNumbers) / sizeof(adminNumbers[0]); i++) {
+        if (senderNumber == adminNumbers[i]) {
+          isAdmin = true;
+          break;
+        }
+      }
+
+      if (isAdmin) {
+        if (!sms.isEmpty()) {
+          // Process the SMS command based on its content
+          if (sms.indexOf("On") != -1) {
+            armAlarm(); // Turn on the alarm
+          } else if (sms.indexOf("Off") != -1) {
+            disarmAlarm(); // Turn off the alarm
+          } else if (sms.indexOf("Battery") != -1) {
+            // Get the battery level and send it as an SMS response
+            int batteryLevel = getBatteryStatus();
+            if (!isnan(batteryLevel)) {
+              sendSMS("Battery level: " + String(batteryLevel) + " %");
+            } else {
+              sendSMS("Unable to retrieve battery level.");
+            }
+          } else {
+            sendSMS("Invalid input. Please enter 'On', 'Off', or 'Battery'."); // Send an SMS with an invalid input message
+          }
+        }
       } else {
-        // Ignore messages from non-admins
+        sendSMS("Unauthorized sender. Phone: " + senderNumber); // Sending SMS that the number is not authorized
       }
     }
   }
 }
 
 
-void processAdminCommand(String command) {
-  // Process the admin command based on the received command string
-  // Check if the command contains "On"
-  if (command.indexOf("On") >= 0) {
-    armAlarm();  // Call the armAlarm() function to arm the alarm
-  }
-  // Check if the command contains "Off"
-  else if (command.indexOf("Off") >= 0) {
-    disarmAlarm();  // Call the disarmAlarm() function to disarm the alarm
-  }
-  // Check if the command contains "Battery"
-  else if (command.indexOf("Battery") >= 0) {
-    checkBatteryStatus();  // Call the checkBatteryStatus() function to check the battery status
-  }
-  // Unknown command
-  else {
-    // Send an SMS with an error message indicating the unknown command
-    sendSMS("Unknown command: " + command + " Please enter 'On', 'Off', or 'Battery'");
-  }
-}
-
-
+//Arming function
 void armAlarm() {
   isArmed = true;  // Set the alarm status to armed
   digitalWrite(ledPin, HIGH);  // Turn on the LED
   sendSMS("Alarm armed");  // Send an SMS notification that the alarm is armed
- }
-
-
-void checkMotionSensor() {
-  // Check if the alarm is armed and the motion sensor is triggered
-  if (isArmed && !isMotionDetected && digitalRead(motionPin) == HIGH) {
-    // Motion sensor detected motion
-    isMotionDetected = true;  // Set the motion sensor status to true
-    digitalWrite(buzzerPin, LOW);  // Turn off the buzzer
-    digitalWrite(ledPin, HIGH);  // Turn on the LED
-    sendSMS("Alarm! Motion detected");  // Send an SMS notification for motion detection
-  }
-  // If motion is detected, blink the LED and the buzzer
-  if (isMotionDetected) {
-    digitalWrite(ledPin, !digitalRead(ledPin));  // Toggle the LED state
-    delay(500);  // Delay for 500 milliseconds
-    digitalWrite(buzzerPin, !digitalRead(buzzerPin));  // Toggle the buzzer state
-    delay(100);
-  }
 }
 
 
@@ -148,53 +160,55 @@ void disarmAlarm() {
   sendSMS("Alarm disarmed");  // Send an SMS notification that the alarm is disarmed
 }
 
-void checkBatteryStatus() {
-  // Send AT command to check battery status
-  gsmModule.println("AT+CBC\r\n");
-  delay(1000);
-  // Read the response from the GSM module
-  String response = gsmModule.readStringUntil('\n');
-  response.trim();
-  // Check if the response contains battery information
-  if (response.indexOf("+CBC:") >= 0) {
-    // Find the comma indices
-    int commaIndex1 = response.indexOf(",");
-    int commaIndex2 = response.indexOf(",", commaIndex1 + 1);
-    if (commaIndex1 >= 0 && commaIndex2 >= 0) {
-      // Extract the battery status
-      String batteryLevel = response.substring(commaIndex1 + 1, commaIndex2);
-      // Send the battery status to the admin numbers
-      sendSMS("Battery Status: " + batteryLevel + "%");
-      return;
-    }
+
+// Checks the motion sensor and triggers the alarm if motion is detected
+void checkMotionSensor() {
+  // Check if the alarm is armed and the motion sensor is triggered
+  if (isArmed && !isMotionDetected && digitalRead(motionPin) == HIGH) {
+    // Motion sensor detected motion
+    isMotionDetected = true;  // Set the motion sensor status to true
+    digitalWrite(buzzerPin, LOW);  // Turn off the buzzer
+    digitalWrite(ledPin, HIGH);  // Turn on the LED
+    sendSMS("Alarm! Motion detected");  // Send an SMS notification for motion detection
   }
-  // If battery status retrieval failed, send an error message
-  sendSMS("Failed to retrieve battery status");
+  // If motion is detected, blink the LED and the buzzer
+  if (isMotionDetected) {
+    digitalWrite(ledPin, !digitalRead(ledPin));  // Toggle the LED state
+    delay(500);  
+    digitalWrite(buzzerPin, !digitalRead(buzzerPin));  // Toggle the buzzer state
+    delay(100); 
+  }
 }
 
-void monitorBatteryLevel() {
-  // Send AT command to check battery status
-  gsmModule.println("AT+CBC\r\n");
-  delay(1000);
-  // Read the response from the GSM module
-  String response = gsmModule.readStringUntil('\n');
-  response.trim();
-  // Check if the response contains battery information
-  if (response.indexOf("+CBC:") >= 0) {
-    // Find the comma indices
-    int commaIndex1 = response.indexOf(",");
-    int commaIndex2 = response.indexOf(",", commaIndex1 + 1);
-    if (commaIndex1 >= 0 && commaIndex2 >= 0) {
-      // Extract the battery level
-      String batteryLevelStr = response.substring(commaIndex2 + 1);
-      int batteryLevel = batteryLevelStr.toInt();
-      // Check if battery level is critical
-      if (batteryLevel <= 10) {
-        sendSMS("Battery level is critical: " + batteryLevelStr + "%");
-      }
-      return;
+
+// Retrieves the battery status from the GSM module
+int getBatteryStatus() {
+  gsmModule.println("AT+CBC");  // Send the command to request battery status
+  unsigned long currentTime = millis();  // Get the current time
+  String batteryResponse = "";  // Variable to store the battery response
+  while (millis() - currentTime < waitTime) {
+    if (gsmModule.available()) {
+      batteryResponse += gsmModule.readStringUntil('\n');  // Read the response from the GSM module
     }
   }
-  // If battery status retrieval failed, send an error message
-  sendSMS("Failed to retrieve battery status");
+  if (batteryResponse.indexOf("+CBC:") >= 0) {
+    // Extract the battery level from the response
+    const int commaIndex1 = batteryResponse.indexOf(',');  // Find the first comma
+    const int commaIndex2 = batteryResponse.indexOf(',', commaIndex1 + 1);  // Find the second comma
+    const String batteryLevelStr = batteryResponse.substring(commaIndex1 + 1, commaIndex2);  // Extract the battery level string
+    return batteryLevelStr.toInt();  // Convert the battery level string to an integer and return it
+  } else {
+    sendSMS("Unable to read battery level");  // Send an SMS notification if unable to read battery level
+    return -1;  // Return -1 to indicate an error
+  }
+}
+
+
+// Checks the critical battery level at a specified interval.
+void checkBattery() {
+    int batteryLvl = getBatteryStatus(); // Get the current battery level
+    if (batteryLvl >= 0 && batteryLvl < 10) {
+      // Battery level is critically low
+      sendSMS("Battery level is critically low at " + String(batteryLvl) + " %. Please recharge the device.");
+    } 
 }
